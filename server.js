303f016943d907fc4b8f6b03d816a413e0486962
Trevor Bos <trevor.bos@outlook.com>
2022-08-15 13:35:37 -0400

    Added the connection information getting the database from postgres. Also moved the connection information into a different folder config and then into the database file.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const expressHbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Database connection
+const db = require('./config/database');
+
+// Test the database
+db.authenticate()
+  .then(() => console.log("Database has been connected!"))
+  .catch((err) => console.log("Here is the error" + err));
 
 const app = express();
 
-app.get('/', (req, res) => res.send("INDEX"));
+app.get("/", (req, res) => res.send("INDEX"));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log (`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
