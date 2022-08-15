14160cd0b691ffd940fa4dee93f29417fcfde40b
Trevor Bos <trevor.bos@outlook.com>
2022-08-15 16:08:53 -0400

    Added the home index page, found a random image from wallhave for a pleasing background.

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ const app = express();
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set("view engine", "handlebars");
 
+// Body Parser
+app.use (bodyParser.urlencoded({ extended: false }));
+
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.send("INDEX"));
+// Index routing
+app.get("/", (req, res) => res.render("index", {layout: 'landing'}));
 
 // Job routing
 app.use("/jobs", require("./routes/jobRoutes"));
